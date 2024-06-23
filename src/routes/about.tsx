3c71d11c6635c950bebceece5f17bdcae49309eb
Chris Birster <christopher.birster@gmail.com>
2024-06-23 15:22:31 -0400

    cleanup

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,24 +1,21 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
-
 export default function About() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">About Page</h1>
-      <Counter />
-      <p class="mt-8">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        About Page
+      </h1>
+      <p class="mt-8">I wanted to learn how to use Solid Start</p>
+      <p class="mt-8">Inspired by the OG</p>
+      <p>
         Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
+        <a
+          href="https://github.com/t3dotgg/roundest-mon"
+          target="_blank"
+          class="text-sky-600 hover:underline"
+        >
+          roundest-mon
         </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <span>About Page</span>
+        to see the original project
       </p>
     </main>
   );
